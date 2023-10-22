f4da3473a482bb9b3f3b21bcceb3776dbe175ba3
Jie Burillo <benjieburillo10@gmail.com>
2023-10-22 09:30:27 +0800

    adding comments

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,14 +6,18 @@ import (
 )
 
 func main() {
+	// Creating a new ServeMux and registering the file server
 	mux := http.NewServeMux()
 	fileServer := http.FileServer(http.Dir("./static"))
 
+	// Registering routes and handlers
 	mux.Handle("/", fileServer)
 	mux.HandleFunc("/form", formHandler)
 	mux.HandleFunc("/hello", helloHandler)
 
 	log.Printf("Starting server at port 8080\n")
+
+	// Removing if statement and using log.Fatal instead
 	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
 }
